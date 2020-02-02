dd37596a6281c8927c52f71fbc63020a28c88c38
nwatabou <wataru1.11@gmail.com>
2020-02-02 17:15:07 +0900

    Initial Commit

---
+++ TestCurrentLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCurrentLocation
+//
+//  Created by nancy on 2020/02/02.
+//  Copyright © 2020 nancy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
