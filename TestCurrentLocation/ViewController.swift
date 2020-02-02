e45340b8cb947b08154cb9494190983382d11170
nwatabou <wataru1.11@gmail.com>
2020-02-02 17:37:36 +0900

    GoogleMapの表示を実装

--- TestCurrentLocation/ViewController.swift
+++ TestCurrentLocation/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    var mapView = GMSMapView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMap()
     }
 
-
+    private func setupMap() {
+        // GoogleMapの初期位置(仮で東京駅付近に設定)
+        let camera = GMSCameraPosition.camera(withLatitude: 35.6812226, longitude: 139.7670594, zoom: 12.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+    }
 }
 
